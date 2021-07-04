81829c7d48054fe79eb0661f26c75c3df20a7d11
Veronica <veronicasmigielski@gmail.com>
2021-07-04 19:27:51 +0200

    Finished the personal experience

---
+++ src/data.js
@@ -0,0 +1,51 @@
+const people = [
+    {
+        id: 1,
+        order:5,
+        title:"React Web Developer Intern",
+        dates:"June 2021 - Present",
+        duties:
+        ["Learned media queries extensively","Worked on the company website using React"],
+        company: "KIDS GALAXY",
+    },
+    
+    {
+        id: 2,
+        order:4,
+        title:"Freelance Web Development",
+        dates:"May 2021 - Present",
+        duties:
+        ["Worked with one client on two websites -- CzechK9 and Castle of Presa", "Volunteer at USYF (United States Youth Forum)"],
+        company:"SELF"
+    },
+
+    {
+        id: 3,
+        order:3,
+        title:"Training Program Alumni",
+        dates:"February 2021 - April 2021",
+        duties:
+        ["Learned Java, Spring Boot, MySQL, jQuery, and React within a 6 week program","Created full-stack web applications as well as simple applications and databases"],
+        company:"MTHREE"
+    },
+
+    {
+        id: 4,
+        order:2,
+        title:"Warehouse Worker",
+        dates:"May 2018 - July 2020",
+        duties:["Learning and using the Oracle products given (Retek Merchandising System, RWMS-LP, and Discovery Viewer)", "Managing inventory for distribution to ten stores throughout overseas NEX locations", "Teaching new hires how to use the RDM system that processes requests between picking, putting away, and receiving items", "Communicating to all departments and leads throughout the process from receiving to storing to shipping goods"],
+        company:"NEXCOM",
+    },
+
+    {
+        id: 5,
+        order:1,
+        title:"Team Lead",
+        dates:"November 2015 - March 2018",
+        duties:["Average daily $4k in transactions/deposits", "Managing the staff on shift to maintain a continuance of flow and customer service", "Preparing the orders of stock required", "Inventory the store to keep track of sales versus damages to create a game plan weekly"],
+        company:"JERSEY MIKES",
+    },
+];
+  
+  export default people;
